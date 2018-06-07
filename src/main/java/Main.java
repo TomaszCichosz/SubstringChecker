2ce7bef54f8f1c0fa9e6cb71dc42a14aee9e7e9b
Tomasz Cichosz <tomaszcichosz@gmail.com>
2018-06-07 18:24:25 +0200

    refactoring

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,41 +6,40 @@ import java.io.PrintWriter;
 public class Main {
 
     public static void main(String[] args) throws IOException { //http://pl.spoj.com/problems/AL_13_10/
-        String stringToCheck, substring, line;
+        String line;
         String[] lineElements;
-        int substringElementsIndex;
-        boolean no;
 
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         PrintWriter writer = new PrintWriter(System.out);
 
         int t = Integer.parseInt(reader.readLine());
         for (int i = 0; i < t; i++) {
-            no = true;
             line = reader.readLine();
             lineElements = line.split("\\s");
-            if (lineElements[0].length() >= lineElements[1].length()) {
-                stringToCheck = lineElements[0];
-                substring = lineElements[1];
-                substringElementsIndex = 0;
-                String[] stringToCheckElements = stringToCheck.split("");
-                String[] substringElements = substring.split("");
-                for (int j = 0; j < stringToCheckElements.length; j++) {
-                    if (stringToCheckElements[j].equals(substringElements[substringElementsIndex])) {
-                        if (substringElementsIndex < substringElements.length - 1) {
-                            substringElementsIndex++;
-                        } else {
-                            writer.println("Tak");
-                            no = false;
-                            j = stringToCheckElements.length;
-                        }
-                    }
-                }
-            }
-            if (no) {
+            if (substringCheck(lineElements[0], lineElements[1])) {
+                writer.println("Tak");
+            } else {
                 writer.println("Nie");
             }
         }
         writer.close();
+    }
+
+    private static boolean substringCheck(String stringToCheck, String substring) {
+        if (stringToCheck.length() >= substring.length()) {
+            String[] stringToCheckElements = stringToCheck.split("");
+            String[] substringElements = substring.split("");
+            int substringElementsIndex = 0;
+            for (String stringToCheckElement : stringToCheckElements) {
+                if (stringToCheckElement.equals(substringElements[substringElementsIndex])) {
+                    if (substringElementsIndex < substringElements.length - 1) {
+                        substringElementsIndex++;
+                    } else {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
     }
 }
